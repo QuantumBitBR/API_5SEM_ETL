19072947afaacd11cb7c1e35ec289c9b52f9624e
João Matheus Lamão <joaomatheuslamao9@gmail.com>
2025-03-08 21:09:41 -0300

    docs(main) - scrum-21: add sample to use taiga methods

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
+from config.database import Database
 from extract.projects import get_projects
+from extract.user_storys import get_user_storys_by_project
 
 
 def main():
-    """Executa a extração de dados do Taiga."""
+    """main."""
+
+    db = Database()
+    
+    print(db.health_check())
+    
+    
     projects = get_projects()
     print(f"Extraídos {len(projects)} projetos.")
-    print("=" * 40)
     for project in projects:
-        print(project)
+        print(project["name"])
+        user_storys = get_user_storys_by_project(project["id"])
+        print(f"Extraídos {len(user_storys)} user stories.")
+        for user_story in user_storys:
+            print(user_story)
+
         print("-" * 40)
+
 
 if __name__ == "__main__":
     main()
