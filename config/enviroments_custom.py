d7f5095ebf29dbca0d39e258b4ecd470ebfbef09
João Matheus Lamão <joaomatheuslamao9@gmail.com>
2025-03-08 21:07:50 -0300

    feat(database) - scrum-21: add database connection with postgresql

--- config/enviroments_custom.py
+++ config/enviroments_custom.py
@@ -2,13 +2,14 @@ import os
 
 from dotenv import load_dotenv
 
-# Carrega as variáveis do .env
 load_dotenv()
 
-# API Taiga
 TAIGA_API_URL = os.getenv("TAIGA_API_URL")
 TAIGA_USERNAME = os.getenv("TAIGA_USERNAME")
 TAIGA_PASSWORD = os.getenv("TAIGA_PASSWORD")
 
-# Banco de Dados
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_USER = os.getenv("DATABASE_USER")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+DATABASE_HOST = os.getenv("DATABASE_HOST")
+DATABASE_PORT = os.getenv("DATABASE_PORT")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
