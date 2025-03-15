93559099a85584330b0684c4328c83f607283444
João Matheus Lamão <joaomatheuslamao9@gmail.com>
2025-03-15 20:27:40 -0300

    feat(count) - scrum-22: implement algorithm to count the number of user stories by card

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src.config.database import Database
+from src.map.fact_user_storie import upsert_fact_user_storie
 from src.map.projects import upsert_all_projects
 from src.map.tags import upsert_all_tags
 from src.utils.logger import Logger
@@ -14,6 +15,7 @@ def main():
 
     upsert_all_projects()
     upsert_all_tags()
+    upsert_fact_user_storie()
 
     Logger.info("Finished ETL process")
 
--- src/extract/user_storys.py
+++ src/extract/user_storys.py
@@ -1,11 +1,17 @@
+import logging
+
 from src.config import config
+from src.utils.logger import Logger
 
 
 def get_user_storys_by_project(id_project):
     """Returns a list of user stories filtered by project."""
-
-    client = config.TaigaClient()
-
-    user_stories = client.api.user_stories.list(project=id_project)
-
-    return [vars(user_story) for user_story in user_stories]
+    try:
+        client = config.TaigaClient()
+        user_stories = client.api.user_stories.list(project=id_project)
+        return [vars(user_story) for user_story in user_stories]
+    except Exception as e:
+        Logger.error(
+            f"An error occurred while fetching user stories for project {id_project}: {e}"
+        )
+        return []
