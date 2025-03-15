98c14e2e9778274b73b8bbc8fb615eb48bca10cc
João Matheus Lamão <joaomatheuslamao9@gmail.com>
2025-03-15 15:23:43 -0300

    feat(logger) - scrum-22: add loggers

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
 from src.config.database import Database
-from src.extract.projects import get_all_projects
-from src.extract.user_storys import get_user_storys_by_project
 from src.map.projects import upsert_all_projects
+from src.map.tags import upsert_all_tags
+from src.utils.logger import Logger
 
 
 def main():
     """main."""
 
-    db = Database()
+    
+    Logger.initialize("etl_process")
 
+    db = Database()
     print(db.health_check())
 
     upsert_all_projects()
-
-    # projects = get_all_projects()
-    # print(f"Extraídos {len(projects)} projetos.")
-    # for project in projects:
-    #     print(project["name"])
-    #     user_storys = get_user_storys_by_project(project["id"])
-    #     print(f"Extraídos {len(user_storys)} user stories.")
-    #     for user_story in user_storys:
-    #         print(user_story)
-
-    #     print("-" * 40)
+    upsert_all_tags()
+    
+    Logger.info("Finished ETL process")
 
 
 if __name__ == "__main__":
